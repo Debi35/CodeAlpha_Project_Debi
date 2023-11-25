e89c3b65a8b1f32134d6cd4844581a5a1cee0c73
DEBI BANERJEE <150055499+Debi35@users.noreply.github.com>
2023-11-25 16:25:56 +0530

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,24 @@
+public class Account{
+    private double balance;
+
+    public Account() {
+        balance = 0.0;
+    }
+
+    public void deposit(double amount){
+        balance = balance + amount;
+    }
+
+    public void withdraw(double amount){
+        if(amount<=balance){
+            balance= balance - amount;
+        }
+        else{
+            System.err.println("Transaction cancelled due to insufficient funds");
+        }
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+}
